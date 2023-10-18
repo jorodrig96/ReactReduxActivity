e340b25a1fc80438146ec14231b8abf70bae2ae4
jorodrig96 <jo.rodrig96@gmail.com>
2023-10-17 19:18:36 -0700

    useSelector, createSlice, useDispatch

---
+++ src/components/Counter.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { increment, decrement, incrementByAmount } from "../ducks/counterSlice"
+
+
+
+export default function Counter () {
+
+    const [ input, setInput ] = useState(0)
+    const count = useSelector(state => state.value)
+    const dispatch = useDispatch()
+
+    const byAmountSubmit = (e) => {
+        e.preventDefault()
+        dispatch(incrementByAmount(Number(input)))
+    }
+
+    return (
+        <div>
+        <h1>{count}</h1>
+        <button onClick = {() => dispatch (decrement())}> Decrement</button>
+        <button onClick = {() => dispatch (increment())}> Increment</button>
+        <form onSubmit={(e) => byAmountSubmit(e)}>
+            <input type= "number" onChange={(e) => setInput(e.target.value)} />
+                <button type= 'submit'>Submit</button>
+        </form>
+        </div>
+    )
+}
